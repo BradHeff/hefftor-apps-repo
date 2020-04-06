9c3571880cd82a052bb47df12da4f168d4e47e57
Brad Heffernan <ph3onix83@gmail.com>
2020-04-07 09:27:19 +0930

    updated

--- x86_64/update_repo.sh
+++ x86_64/update_repo.sh
@@ -3,13 +3,13 @@
 echo "Clean out old db"
 echo
 
-rm hefftor-repo*
+rm hefftor-apps-repo*
 
 echo
 echo "Run repo-add"
 echo 
 
-repo-add hefftor-repo.db.tar.gz *.pkg.tar.zst
+repo-add hefftor-apps-repo.db.tar.gz *.pkg.tar.zst
 
 echo "####################################"
 echo "Repo Updated!!"
